e81c13a3aa583e8a8a548856e2e9fb32754c6b23
Grey <inertgrey@outlook.com>
2019-07-18 23:22:52 +0200

    Removed unused class

--- Task02/app/src/main/java/com/arkdev/task02/activities/MainActivity.java
+++ Task02/app/src/main/java/com/arkdev/task02/activities/MainActivity.java
@@ -1,28 +1,14 @@
 package com.arkdev.task02.activities;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.TextView;
 
 import com.arkdev.task02.R;
 import com.arkdev.task02.adapters.MovieAdapter;
-import com.arkdev.task02.models.Book;
 import com.arkdev.task02.models.Movie;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
